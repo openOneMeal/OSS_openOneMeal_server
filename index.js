f96cdec4895bab7c223dce2e72539e1c9f053cb7
hmusk7 <hmusk7@gmail.com>
2024-05-27 01:52:07 +0900

    fix: cors 명시적 URL 설정

--- index.js
+++ index.js
@@ -12,12 +12,17 @@ mongoose.connect(dbUri)
     .catch(err => console.log(err));
 
 app.use(express.json());
+
 // 클라이언트가 서버로 요청을 보내기 이전에, 사전 요청을 보냄.
 // 사전 요청은 CORS Preflight Reqeust 라고 하며, OPTIONS 메서드로 사전 요청을 보내고,
 // 이 요청은 서버가 특정 HTTP 메서드(POST, UPDATE 등)를 허용하는지 확인함
 // 이 요청에 적절히 응답해야 클라이언트가 실제 요청을 보내기 때문에,
 // CORS 미들웨어를 추가하여 반드시 이 요청을 처리해줘야함.
-app.use(cors());
+app.use(cors({
+    origin: 'https://openonemeal.github.io'
+}));
+app.options('/api/signin', cors());
+app.options('/api/signup', cors());
 
 // 테스트 시에는 3000, 4000 등 임시 포트 사용
 // 배포 시에는 80(HTTP), 또는 443(HTTPS) 포트 사용
