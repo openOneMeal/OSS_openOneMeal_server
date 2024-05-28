40ebd2f7d44af5766a90f774624b13f321df46b2
hmusk7 <hmusk7@gmail.com>
2024-05-28 22:37:34 +0900

    fix: server 에도 cors 처리 (2)

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
         origin: '*',
-        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+        methods: ['GET', 'POST']
     }
 });
 
